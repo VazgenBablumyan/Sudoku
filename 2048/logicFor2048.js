65dd68a65d250817ef316a4b674a79a6f5f3453d
Mane Manesyan <manemanesyan062@gmail.com>
2023-05-02 22:43:04 +0400

    Add the function for moving up rows.

--- 2048/logicFor2048.js
+++ 2048/logicFor2048.js
@@ -4,3 +4,35 @@ let grid=[
     [0,0,0,0],
     [0,0,0,0]
 ]
+for (let i = 0; i < 2; i++) {
+    let row = Math.floor(Math.random() * 4)
+    let col = Math.floor(Math.random() * 4)
+    let num = 2 
+    if (grid[row][col] === 0) { 
+      grid[row][col] = num
+    } else { 
+      i--
+    }
+  }
+function moveUp(){
+  let currentRow=0
+for(let i=0;i<grid.length;i++){
+  for(let j=1;j<grid.length;j++){
+   if(grid[i][j]!==0){
+    if(grid[currentRow][j]===0)
+    grid[currentRow][j]=grid[i][j]
+    grid[i][j]=0
+   }else if(grid[currentRow][j]===grid[i][j]){
+    grid[currentRow][j]*=2
+    grid[i][j]=0
+    currentRow++
+   }else{
+    currentRow++
+    grid[currentRow][j]=grid[i][j]
+    grid[i][j]=0
+   }
+
+  }
+}
+return grid
+}
