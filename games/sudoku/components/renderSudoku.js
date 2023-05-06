151b6fb284f2f58b39d5a4381e18c1e59973c270
VazgenBablumyan <vazgeb@gmail.com>
2023-05-06 19:24:48 +0400

    feat: end render sudoku

--- games/sudoku/components/renderSudoku.js
+++ games/sudoku/components/renderSudoku.js
@@ -1,16 +1,14 @@
 import { createElement } from "../helper/createElement.js";
 
 createElement("section", { class: "sudoku" });
+const bigTab = createElement("section", { class: "sudoku" },);
 
-const bigTab = createElement("section", { class: "default" });
-const square = createElement("div", { class: "square" });
-const box = createElement("div", { class: "boxStyle" });
 
 export function renderSudoku() {
-  for (let i = 0; i < 9; i++) {
-    bigTab.appendChild(square);
+  for(let i = 1; i < 10; i++) {
+    let currentSquare = bigTab.appendChild(createElement("div", { class: "square", id: "square"+ i }));
     for (let idx = 0; idx < 9; idx++) {
-      square.appendChild(box);
+      currentSquare.appendChild(createElement("div", { class: "boxStyle" })  );
     }
   }
   return bigTab;
--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import { CreateHeader } from "./games/sudoku/components/header.js"
 import { renderSudoku } from "./games/sudoku/components/renderSudoku.js"
 
 const root = document.getElementById("root")
-root.appendChild(CreateHeader())
-root.appendChild(Play)
-root.appendChild(renderSudoku())
+root.append(CreateHeader())
+root.append(Play)
+root.append(renderSudoku())
