a8579ae480b7c35a5ff7503f4cb715799ff41e79
VazgenBablumyan <vazgeb@gmail.com>
2023-05-07 12:07:12 +0400

    add classes to box

--- games/sudoku/components/renderSudoku.js
+++ games/sudoku/components/renderSudoku.js
@@ -1,17 +1,20 @@
 import { createElement } from "../helper/createElement.js";
 
-createElement("section", { class: "sudoku" });
-const section = createElement("section", { class: "sudoku" },);
+
+const section = createElement("section", { class: "sudoku" });
+
+
 
 export function renderSudoku() {
   for(let i = 1; i < 10; i++) {
     const square = createElement("div", { class: "square", id: "square"+ i })
+    section.appendChild(square)
     for (let idx = 0; idx < 9; idx++) {
-      const box = createElement("div", { class: "boxStyle"},5) 
+      const box = createElement("div", { class: "boxStyle manualWrite defaultWrite"},5) 
       square.appendChild(box);
     }
     
-    bigTab.appendChild(section);
+    section.appendChild(square);
   }
-  return bigTab;
+  return section;
 }
